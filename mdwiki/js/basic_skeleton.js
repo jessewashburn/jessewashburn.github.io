3ba650186cae36288170182ab658a674b40d9a54
Jesse Washburn <jeswashburn@gmail.com>
2025-01-07 14:07:58 -0500

    another possible fix

--- mdwiki/js/basic_skeleton.js
+++ mdwiki/js/basic_skeleton.js
@@ -169,33 +169,35 @@
     }
 
     function addInpageAnchors() {
-        $('h1,h2,h3,h4,h5,h6').not('#md-title h1').each(function () {
-            var $heading = $(this);
-            $heading.addClass('md-inpage-anchor');
-            var text = $heading.text().trim();
-            var href = '#' + text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '');
+        // adds a link to the navigation at the top of the page
+        function addJumpLinkToTOC($heading) {
+            if ($.md.config.useSideMenu === false) return;
+            if ($heading.prop("tagName") !== 'H2') return;
+    
+            var c = $.md.config.tocAnchor;
+            if (c === '') return;
+    
+            // Ensure the anchor href is correctly interpreted
+            var baseHref = window.location.pathname.split('#')[0]; // Get current base path without hash
+            var $jumpLink = $('<a class="visible-xs visible-sm jumplink" href="' + baseHref + '#md-page-menu">' + c + '</a>');
             
-            // Use an absolute URL if necessary
-            var baseUrl = window.location.origin + window.location.pathname.split('#')[0];
-            var absoluteHref = baseUrl + href;
-            
-            console.log('Generated anchor:', absoluteHref); // Debugging
-            var $anchor = $('<a class="anchor-highlight" href="' + absoluteHref + '">¶</a>');
-            $heading.append($anchor);
+            $jumpLink.click(function (ev) {
+                ev.preventDefault();
+    
+                $('body').scrollTop($('#md-page-menu').position().top);
+            });
+    
+            if ($heading.parents('#md-menu').length === 0) {
+                $jumpLink.insertAfter($heading);
+            }
+        }
+    
+        // Call the TOC link addition for all H2 headings
+        $('h2').each(function () {
+            addJumpLinkToTOC($(this));
         });
     }
     
-
-        // adds a page inline anchor to each h1,h2,h3,h4,h5,h6 element
-        // which can be accessed by the headings text
-        $('h1,h2,h3,h4,h5,h6').not('#md-title h1').each (function () {
-            var $heading = $(this);
-            $heading.addClass('md-inpage-anchor');
-
-            //add jumplink to table of contents
-            addJumpLinkToTOC($heading);
-        });
-    }
 
 	function externalLinksTargetBlank () {
 		$("a[href^='http://']").each(
